53743dc870f66b90724fb422017cd3513247f8e9
Your Name <you@example.com>
2015-10-02 11:40:31 +0000

    update

--- hostapd_setup.sh
+++ hostapd_setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-sudo mv ./setup/interfaces/ /etc/network/
+sudo cp -r ./setup/interfaces/ /etc/network/
 
 sudo apt-get install hostapd
 
-sudo echo 'alias hostapd="hostapd ~/hostapd_setup/setup/hostapd.conf'
+echo 'alias hostapd="hostapd ~/hostapd_setup/setup/hostapd.conf"' >> ~/.bashrc
+
 
